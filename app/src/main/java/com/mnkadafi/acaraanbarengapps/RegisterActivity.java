edd0528491c0b4c86d31500e1111c541fa6534ea
Mochamad Nurkhayal Kadafi <wowkadafi@gmail.com>
2022-08-04 00:21:37 +0700

    first initialization

---
+++ app/src/main/java/com/mnkadafi/acaraanbarengapps/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.mnkadafi.acaraanbarengapps;
+
+// Tanggal 4 Agustus 2022
+// NIM  : 10119197
+// Nama : Mochamad Nurkhayal Kadafi
+// Kelas: IF-5
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
